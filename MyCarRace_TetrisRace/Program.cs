c7bc6613cd6667844b55071dbf90357514822413
mic.armenia <mic.armenia@hotmail.com>
2019-04-11 13:59:43 +0400

    Add TetrisRace

---
+++ MyCarRace_TetrisRace/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+using static System.Console;
+namespace MyCarRace_TetrisRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            do
+            {
+                char a = (Char)i;
+                Car c = new Car(a);
+                c.Play();
+                i++;
+            } while (ReadKey().Key == ConsoleKey.Enter);
+
+        }
+    }
+}
